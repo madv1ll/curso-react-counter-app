93fdbb7f2af0300065ab5d15af57984ab14c3255
madv1ll <kevint.mtn@gmail.com>
2022-02-19 19:49:41 -0300

    Pruebas: 07-deses-arr

---
+++ src/tests/base/07-deses-arr.test.js
@@ -0,0 +1,15 @@
+import { retornaArreglo } from "../../base/07-deses-arr"
+
+describe('Pruebas en desestructuracion', () => { 
+    test('Debe retornar un string y un numero', () => {
+        const [letras, numeros] = retornaArreglo();
+
+        // expect(arr).toEqual(['ABC', 123])
+        // console.log(typeof letras)
+        expect(letras).toBe('ABC');
+        expect(typeof letras).toBe('string');
+        
+        expect(numeros).toBe(123);
+        expect(typeof numeros).toBe('number');
+    })
+ })
